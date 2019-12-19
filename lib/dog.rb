1b3a222e8569597053a3bd8fb56bb6a617c90f49
Ayako Wiltshire <awittyl44@gmail.com>
2019-12-19 06:55:09 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,18 @@
 
 
 class Dog 
-  attr_accessor :name
+  attr_accessor :name, :breed
   
   def initialize(name, breed)
     @name = name 
+    @breed = breed
   end
   
-  def breed=(breed)
-    @breed = breed 
-  end
+  # def breed=(breed)
+  #   @breed = breed 
+  # end
   
-  def breed
-    @breed
-  end
+  # def breed
+  #   @breed
+  # end
 end
